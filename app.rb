b5cbec2e5a941ab356a7edf57c55420057ae06f2
anderscodes <anders.j.persson@hotmail.com>
2018-05-01 14:57:56 +0100

    feature form

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 get '/' do
-  'Testing infrastructure working!'
+  erb(:index)
+end
+
+post '/play' do
+  @name1 = params[:name1]
+  @name2 = params[:name2]
+  erb(:play)
 end
 
 run! if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,17 @@
 feature Battle do
-  scenario 'should load requested string' do
-    visit ('/')
-    expect(page).to have_content('Testing infrastructure working!')
+  # before :each do
+  #   Player1.make(name: 'Habin')
+  #   Player2.make(name: 'Anders')
+  # end
+
+  scenario 'should return name' do
+    visit ('/index')
+    # expect(page).to have_content('This is a form')
+    within("/") do
+      fill_in 'name1', with: 'Habin'
+      fill_in 'name2', with: 'Anders'
+    end
+    click_button 'submit'
+    expect(page).to have_content('Player one is called Habin. Player two is called Anders.')
   end
 end
