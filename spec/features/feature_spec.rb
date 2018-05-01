29bf97919564ad913f4f7ccbe02d4ae5810e5285
Habin <habin@hotmail.co.uk>
2018-05-01 14:06:13 +0100

    change syntax of feature test for capybara

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,6 @@
-describe Battle, type: :feature do
-  it 'should load requested string' do
-    visit '/'
+feature Battle do
+  scenario 'should load requested string' do
+    visit ('/')
     expect(page).to have_content('Testing infrastructure working!')
   end
 end
