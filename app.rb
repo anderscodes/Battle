ddd8db9416053336351c8c5b9156f82474b9fc54
Habin <habin@hotmail.co.uk>
2018-05-01 17:11:44 +0100

    test for redirecting form to play page passes

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 get '/' do
   erb(:index)
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -5,12 +5,10 @@ feature Battle do
   # end
 
   scenario 'should return name' do
-    visit ('/index')
+    visit ('/')
     # expect(page).to have_content('This is a form')
-    within("/") do
       fill_in 'name1', with: 'Habin'
       fill_in 'name2', with: 'Anders'
-    end
     click_button 'submit'
     expect(page).to have_content('Player one is called Habin. Player two is called Anders.')
   end
