f04777ce6de725ca2575103cacc1f5a74a62aa49
anderscodes <anders.j.persson@hotmail.com>
2018-05-02 15:38:46 +0100

    written test for player 1 attack and html/css for play

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,16 +3,27 @@ feature Battle do
   let(:player_name_1) { 'Habin' }
   let(:player_name_2) { 'Anders' }
   let(:hitpoints) { 7 }
+  let(:attack_value) { 3 }
+  let(:game_set_up) { visit_index_add_names_and_submit(player_name_1, player_name_2) }
 
   context 'At the beginning' do
     scenario 'Should return name' do
-      visit_index_add_names_and_submit(player_name_1, player_name_2)
-      expect(page).to have_content('Player one is called Habin. Player two is called Anders.')
+      game_set_up
+      expect(page).to have_content("Player one is called #{player_name_1}. Player two is called #{player_name_2}.")
     end
 
     scenario 'Should return player 2 hit points' do
-      visit_index_add_names_and_submit(player_name_1, player_name_2)
+      game_set_up
       expect(page).to have_content("Player two hit points: #{hitpoints}")
     end
   end
+
+  context 'Player 1 attacks' do
+    scenario 'Should reduce hit point of player 2' do
+      game_set_up
+      click_button('#player_1_attack')
+      expect(page).to have_content("Player two hit points: #{hitpoints - attack_value}")
+    end
+  end
+
 end
